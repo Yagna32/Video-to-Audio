786f56dfb48500202437e6feb175fd45ee3daae5
yagna <yagnapatelhirenk@gmail.com>
2024-04-06 19:55:08 +0530

    minor fix

--- src/gateway/auth/validate.py
+++ src/gateway/auth/validate.py
@@ -1,7 +1,7 @@
 import os,requests
 
 def token(request):
-    if not "Authorization" in request.header:
+    if not "Authorization" in request.headers:
         return None,("missing creds",401)
     token = request.headers['Authorization']
     if not token:
@@ -12,6 +12,6 @@ def token(request):
         headers={"Authorization":token}
     )
     if response.status_code == 200:
-        return response.txt,None
+        return response.text,None
     else: 
-        return None,(response.txt,response.status_code)
+        return None,(response.text,response.status_code)
