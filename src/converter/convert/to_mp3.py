240af5eff2bee1c4b6cb2365296c2079944cd87e
yagna <yagnapatelhirenk@gmail.com>
2024-04-12 07:38:49 +0530

    minor changes

--- src/converter/convert/to_mp3.py
+++ src/converter/convert/to_mp3.py
@@ -1,23 +1,25 @@
 import pika, json, tempfile, os
 from bson.objectid import ObjectId
-import moviepy.editor # type: ignore
+from moviepy.editor import *
 def start(message, fs_videos, fs_mp3s, channel):
     message = json.loads(message)
-
     #empty temp file
-    tf = tempfile.NamedTemporaryFile() #automatically gets deleted
-    #video contents
-    out = fs_videos.get(ObjectId(message["video_fid"]))  #to get the file from mongodb
-    #add video contents to empty file
-    tf.write(out.read())
-    #create audio from temp video file
-    audio = moviepy.editor.VideoFileClip(tf.name).audio
-    tf.close()
+    try:
+        tf = tempfile.NamedTemporaryFile() #automatically gets deleted
+        #video contents
+        out = fs_videos.get(ObjectId(message["video_fid"]))  #to get the file from mongodb
+        #add video contents to empty file
+        tf.write(out.read())
+        #create audio from temp video file
+        audio = VideoFileClip(tf.name).audio
+        tf.close()
 
-    #write the audio to the file
-    tf_path = tempfile.gettempdir()+ f"/{message['video_fid']}.mp3"
-    audio.write_audiofile(tf_path) #creating the temp file for audio
-
+        #write the audio to the file
+        tf_path = tempfile.gettempdir()+ f"/{message['video_fid']}.mp3"
+        audio.write_audiofile(tf_path) #creating the temp file for audio
+    except Exception as e:
+        print(e)
+        return str(e)
     # save the file to mongo
     f = open(tf_path,"rb")
     data = f.read()
